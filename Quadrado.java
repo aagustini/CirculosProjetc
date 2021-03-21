1ba33660070c80b8d3a2c188c6d7330beeab008e
flavia-schuh <flaviats@gmail.com>
2021-03-21 14:07:41 -0300

    Nova figura: quadrado

---
+++ Quadrado.java
@@ -0,0 +1,33 @@
+public class Quadrado implements Figura2D {
+    private double lado;
+    private Ponto cantoSupEsquerdo;
+
+    public Quadrado(double x, double y, double umLado) {
+        cantoSupEsquerdo = new Ponto(x,y);
+        lado = umLado;     
+    }
+
+    @Override
+    public double area() {
+        return Math.pow(lado, 2);
+    }
+
+    @Override
+    public void mover(double umX, double umY) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void zoom(double fator) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(quadr) CSE: %s lado: %.2f",
+                cantoSupEsquerdo.toString(), lado);
+    }
+    
+}
